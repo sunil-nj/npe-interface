b50f3eebe86ccfc9a221c58fe4e62f606b0689f7
shivi <agrwl.shivi@gmail.com>
2022-11-03 21:04:39 +0530

    Accounts and Profile page

--- src/Components/payment/AccountsPage.js
+++ src/Components/payment/AccountsPage.js
@@ -1,7 +1,82 @@
 import Nav3 from "./Navbar";
 
+import Divider from '@mui/material/Divider';
+import "./ProfilePage.css"
+
 function AccountsPage(){
-    return(<body id="accpage"><Nav3/><div>Account details</div></body>)
+    return(<body id="accpage"><Nav3/>
+    <div className="proftitle">ACCOUNT DETAILS</div>
+    <Divider variant="middle" sx={{
+          marginLeft:'4%',
+          marginRight:'4%'
+        }}/>
+        <div className="details">Personal Information</div>
+
+    <table className="details">
+        <tr className="row">
+            <td className="c1">Account Id</td>
+            <td className="c2"> : </td>
+            <td className="c3"> 123456</td>
+        </tr>
+        <tr className="row">
+            <td className="c1">Account Name</td>
+            <td className="c2"> : </td>
+            <td className="c3"> account name 123</td>
+        </tr>
+        <tr className="row">
+            <td className="c1">Address </td>
+            <td className="c2"> : </td>
+            <td className="c3"> Address Line 1</td>
+        </tr>
+        <tr className="row">
+            <td className="c1"> </td>
+            <td className="c2">  </td>
+            <td className="c3"> city</td>
+        </tr>
+        <tr className="row">
+            <td className="c1"> </td>
+            <td className="c2">  </td>
+            <td className="c3"> pincode</td>
+        </tr>
+    </table>
+    <table className="details">
+        <tr className="row">
+            <td className="c1">Total Balance</td>
+            <td className="c2"> : </td>
+            <td className="c3"> 123354</td>
+        </tr>
+        <tr className="row">
+            <td className="c1">Available Balance</td>
+            <td className="c2"> : </td>
+            <td className="c3"> 2365485</td>
+        </tr>
+        <tr className="row">
+            <td className="c1">Blocked Credit Balance</td>
+            <td className="c2"> : </td>
+            <td className="c3"> 7347</td>
+        </tr>
+        <tr className="row">
+            <td className="c1">Blocked Debit Balance</td>
+            <td className="c2"> : </td>
+            <td className="c3"> 87746</td>
+        </tr>
+        <tr className="row">
+            <td className="c1">Account Created Date</td>
+            <td className="c2"> : </td>
+            <td className="c3"> date</td>
+        </tr>
+        <tr className="row">
+            <td className="c1">Account Status</td>
+            <td className="c2"> : </td>
+            <td className="c3"> acc status</td>
+        </tr>
+        <tr className="row">
+            <td className="c1">Account Type</td>
+            <td className="c2"> : </td>
+            <td className="c3"> Savings</td>
+        </tr>
+        </table>
+    </body>)
 }
 
 export default AccountsPage;
--- src/Components/payment/ProfilePage.js
+++ src/Components/payment/ProfilePage.js
@@ -1,7 +1,68 @@
 import Nav3 from "./Navbar";
 
+import Divider from '@mui/material/Divider';
+import "./ProfilePage.css"
+
 function ProfilePage(){
-    return(<body id="profpage"><Nav3/><div>Profile details</div></body>)
+    return(<body id="profpage"><Nav3/>
+    <div className="proftitle">PROFILE DETAILS</div>
+    <Divider variant="middle" sx={{
+          marginLeft:'4%',
+          marginRight:'4%'
+        }}/>
+        <div className="details">Personal Information</div>
+
+    <table className="details">
+        <tr className="row">
+            <td className="c1">Customer Id</td>
+            <td className="c2"> : </td>
+            <td className="c3"> 123456</td>
+        </tr>
+        <tr className="row">
+            <td className="c1">Email Id</td>
+            <td className="c2"> : </td>
+            <td className="c3"> email@xyz</td>
+        </tr>
+        <tr className="row">
+            <td className="c1">Phone Number</td>
+            <td className="c2"> : </td>
+            <td className="c3"> 1234567890</td>
+        </tr>
+        <tr className="row">
+            <td className="c1">Address </td>
+            <td className="c2"> : </td>
+            <td className="c3"> Address Line 1</td>
+        </tr>
+        <tr className="row">
+            <td className="c1"> </td>
+            <td className="c2">  </td>
+            <td className="c3"> city</td>
+        </tr>
+        <tr className="row">
+            <td className="c1"> </td>
+            <td className="c2">  </td>
+            <td className="c3"> pincode</td>
+        </tr>
+    </table>
+    <div className="details">Services</div>
+    <table className="details">
+        <tr className="row">
+            <td className="c1">Product</td>
+            <td className="c2"> : </td>
+            <td className="c3"> product 1</td>
+        </tr>
+        <tr className="row">
+            <td className="c1">Service Name</td>
+            <td className="c2"> : </td>
+            <td className="c3"> service 123</td>
+        </tr>
+        <tr className="row">
+            <td className="c1">Enrollment Status</td>
+            <td className="c2"> : </td>
+            <td className="c3"> status</td>
+        </tr>
+        </table>
+    </body>)
 }
 
 export default ProfilePage;
